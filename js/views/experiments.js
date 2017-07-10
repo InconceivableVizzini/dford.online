67d3ef9012d80345f5945e816de87dbdb83096bd
Derek Ford <inconceivablevizzini@gmail.com>
2017-07-10 03:30:01 -0400

    Add google analytics, add composition paintings, add source highlighting

--- js/views/experiments.js
+++ js/views/experiments.js
@@ -2,18 +2,36 @@ import { riot, router, Router} from '../commonrouter.js';
 
 riot.tag('experiments',
   `
-  <div class="tile is-12 experiment-hero pinhole-hero">
-    <div class="background"></div>
-    <div class="content" onclick={pinhole}>
-      <p>Pinhole, an adventure for Eclipse Phase.</p>
-    </div>
+  <div class="tile is-vertical">
+    <div class="tile">
+      <div class="tile is-parent is-8 is-left-triplet">
+         <article class="tile is-child experiment-hero matte-painting-rv-hero">
+	 <div class="background"></div>
+	 <div class="content" onclick={composition}>
+	 <p>Composition-focused matte paintings.</p>
+	 </div>
+	 </article>
+      </div>
+      <div class="tile is-parent is-vertical is-right-triplet">
+        <article class="tile is-child experiment-hero visualizer-hero">
+          <div class="background"></div>
+          <div class="content" onclick={visualizer}>
+            <p>Audio visualization.</p>
+          </div>
+        </article>
+        <article class="tile is-child experiment-hero pinhole-hero">
+          <div class="background"></div>
+          <div class="content" onclick={pinhole}>
+            <p>Pinhole, an adventure for Eclipse Phase.</p>
+          </div>
+        </article>
+      </div>
+    </div>	 
   </div>
-  <div class="tile is-8 experiment-hero visualizer-hero">
-    <div class="background"></div>
-    <div class="content" onclick={visualizer}>
-      <p>Audio visualization.</p>
-    </div>
-  </div>
+
+
+
+
   `,
 
   function (opts) {
